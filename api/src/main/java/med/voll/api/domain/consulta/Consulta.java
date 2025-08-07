10a4745c47ab594013fc13dcc63c2c78e1474151
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-08-07 13:29:35 -0300

    nueva funcionalidad | agregacion de propiedad baseline para el estado inicial/ punto de partida a partir del cual Flyway comienza a aplicar las migraciones | agregar las consultas DatosDetalleConsulta , DatosReservaConsulta

---
+++ api/src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,35 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)  //muchas consultas pueden estar asociadas a un solo médico
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)  //muchas consultas pueden estar asociadas a un paciente
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime fecha;
+
+}
